9fdf9eb675044e5bec693104fd44ab954f98b7b6
Dreamer <nulva1@yandex.ru>
2023-02-13 13:46:07 +0500

    create ui button

---
+++ components/ui/v-button/v-button.vue
@@ -0,0 +1,35 @@
+<template>
+  <button :class="buttonClass" :disabled="error || disabled">
+    {{ text }}
+  </button>
+</template>
+<script>
+import {computed} from 'vue'
+export default {
+  name: 'v-button',
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    error: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props) {
+    const buttonClass = computed(() => {
+      return ['v-button', {'v-button--error error': props.error}, {'not-focus': props.disabled}]
+    })
+
+    return {
+      buttonClass
+    }
+  }
+}
+</script>
+<style lang="scss" scoped src="./v-button.scss"/>
