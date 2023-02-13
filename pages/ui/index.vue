e05975f0ae07b6bc523a8bf524a35293c60c0777
Dreamer <nulva1@yandex.ru>
2023-02-13 13:46:13 +0500

    create ui-page

---
+++ pages/ui/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="ui">
+    <v-button text="submit"/>
+    <v-button text="submit" error/>
+    <v-button text="submit" disabled/>
+  </div>
+</template>
+<script>
+import VButton from "../../components/ui/v-button/v-button";
+
+export default {
+  name: '',
+  components: { VButton },
+  setup() {
+
+  }
+}
+</script>
+<style lang="scss" scoped src="./ui.scss"/>
